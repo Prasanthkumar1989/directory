ff95d5ef7c52dded748aecbc599ca623f4015a2e
Prasanth Kumar <prasanth.kb5@gmail.com>
2015-03-11 18:45:37 +0530

    save
    save

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -1,4 +1,4 @@
-var serviceURL = "http://localhost/directory/services/";
+var serviceURL = "http://itsprasanth/services/";
 var employee;
 $('#detailsPage').live('pageshow', function(event) {
 	var id = getUrlVars()["id"];
